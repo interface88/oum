25d6adc1032c98ed0bc1faae3bb22e299bcddc34
Rajesh-kakawat <rajesh.kakawat97@gmail.com>
2020-09-25 14:35:52 +0530

    footer changes

--- public/themes/unity/footer.php
+++ public/themes/unity/footer.php
@@ -3,50 +3,51 @@
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-3 col-md-6">
-            <div class="footer-info">
-              <h3>OUM</h3>
-              <p>
-                A108 Adam Street <br>
-                NY 535022, USA<br><br>
-                <strong>Phone:</strong> +1 5589 55488 55<br>
-                <strong>Email:</strong> info@example.com<br>
-              </p>
-              <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
+          
 
           <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
+            <h4>Company</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Join us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Offices</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy & policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Sitemap</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Contact</a></li>
             </ul>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
+            <h4>Categories</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Medical</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Family</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Children</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Education</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Disaster</a></li>
             </ul>
+          </div>
+	
+	<div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h4>HEAD OFFICE</h4>
+              <p>
+                <strong>Address:</strong> 8 King Street, Melbourne Victoria 89<br>
+                <strong>Phone:</strong>+22 6 4567 8900<br>
+              </p>
+            </div>
+            <div class="footer-info">
+              <h4>Brand</h4>
+              <p>
+                <strong>Address:</strong> 2 Queen Street,California, USA<br>
+                <strong>Phone:</strong>+22 6 4567 8900<br>
+              </p>
+            </div>
           </div>
 
           <div class="col-lg-4 col-md-6 footer-newsletter">
             <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <p>Subscribe to receive inspiration, ideas, and news in your inbox.</p>
             <form action="" method="post">
               <input type="email" name="email"><input type="submit" value="Subscribe">
             </form>
@@ -56,17 +57,24 @@
         </div>
       </div>
     </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>OUM</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/restaurantly-restaurant-template/ -->
-        Designed by <a href="https://bootstrapmade.com/"></a>
-      </div>
-    </div>
+	<div class="footer-bottom">
+        <div class="container">
+         <div class="row">
+         	 <div class="col-lg-6 col-md-6">
+                  <div class="copyright">
+                    &copy; Copyright <strong><span>OUM</span></strong>. All Rights Reserved
+                  </div>
+         	 </div>
+         	 <div class="col-lg-6 col-md-6">
+                  <div class="social-links float-right">
+                    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                  </div>
+         	 </div>
+         </div>
+        </div>
+	</div>
   </footer><!-- End Footer -->
